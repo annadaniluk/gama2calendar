c5aa17e75c02fac38d9e4996ae08852f6afb13cf
annadaniluk <114945996+annadaniluk@users.noreply.github.com>
2025-02-05 19:51:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+const button1=document.getElementById("pb");
+const block1=document.getElementById("pid");
+function dactive(){
+    document.getElementById("pid").classList.toggle("dactive");
+}
+button1.addEventListener("click",function(){
+    block1.classList.toggle("active");
+    setTimeout(dactive, 15000); 
+})
+
+function dactive4(){
+    document.getElementById("pid4").classList.toggle("dactive");
+}
+document.getElementById("pb4").addEventListener("click",function(){
+    document.getElementById("pid4").classList.toggle("active");
+    setTimeout(dactive4, 15000);
+})
+
+function dactive6(){
+    document.getElementById("pid6").classList.toggle("dactive");
+}
+document.getElementById("pb6").addEventListener("click",function(){
+    document.getElementById("pid6").classList.toggle("active");
+    setTimeout(dactive6, 15000);
+})
+
+function dactive8(){
+    document.getElementById("pid8").classList.toggle("dactive");
+}
+document.getElementById("pb8").addEventListener("click",function(){
+    document.getElementById("pid8").classList.toggle("active");
+    setTimeout(dactive8, 15000);
+})
+
+function dactive10(){
+    document.getElementById("pid10").classList.toggle("dactive");
+}
+document.getElementById("pb10").addEventListener("click",function(){
+    document.getElementById("pid10").classList.toggle("active");
+    setTimeout(dactive10, 15000);
+})
+
+function dactive12(){
+    document.getElementById("pid12").classList.toggle("dactive");
+}
+document.getElementById("pb12").addEventListener("click",function(){
+    document.getElementById("pid12").classList.toggle("active");
+    setTimeout(dactive12, 15000);
+})
+
+function dactive14(){
+    document.getElementById("pid14").classList.toggle("dactive");
+}
+document.getElementById("pb14").addEventListener("click",function(){
+    document.getElementById("pid14").classList.toggle("active");
+    setTimeout(dactive14, 15000);
+})
+
+function dactive16(){
+    document.getElementById("pid16").classList.toggle("dactive");
+}
+document.getElementById("pb16").addEventListener("click",function(){
+    document.getElementById("pid16").classList.toggle("active");
+    setTimeout(dactive16, 15000);
+})
+
+function dactive18(){
+    document.getElementById("pid18").classList.toggle("dactive");
+}
+document.getElementById("pb18").addEventListener("click",function(){
+    document.getElementById("pid18").classList.toggle("active");
+    setTimeout(dactive18, 15000);
+})
+
+function dactive20(){
+    document.getElementById("pid20").classList.toggle("dactive");
+}
+document.getElementById("pb20").addEventListener("click",function(){
+    document.getElementById("pid20").classList.toggle("active");
+    setTimeout(dactive20, 15000);
+})
+
+function dactive22(){
+    document.getElementById("pid22").classList.toggle("dactive");
+}
+document.getElementById("pb22").addEventListener("click",function(){
+    document.getElementById("pid22").classList.toggle("active");
+    setTimeout(dactive22, 15000);
+})
+
+function dactive24(){
+    document.getElementById("pid24").classList.toggle("dactive");
+}
+document.getElementById("pb24").addEventListener("click",function(){
+    document.getElementById("pid24").classList.toggle("active");
+    setTimeout(dactive24, 15000);
+})
+
